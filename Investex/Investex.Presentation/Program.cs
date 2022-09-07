b89945060bf8fcf0c318e6267c9a7f00f1240c2a
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-09-07 16:00:39 +0300

    .

--- Investex/Investex.Presentation/Program.cs
+++ Investex/Investex.Presentation/Program.cs
@@ -5,9 +5,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("MyConnection"))
+    );
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
