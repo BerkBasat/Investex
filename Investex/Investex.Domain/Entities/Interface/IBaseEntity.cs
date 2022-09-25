5d04e728c8da4964e0e7278bd5c20e44a16a3496
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-09-25 14:29:15 +0300

    First entities created.

---
+++ Investex/Investex.Domain/Entities/Interface/IBaseEntity.cs
@@ -0,0 +1,28 @@
+﻿using Investex.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investex.Domain.Entities.Interface
+{
+    public interface IBaseEntity:IEntity<Guid>
+    {
+        public Status Status { get; set; }
+
+        // Data added
+        public string CreatedDate { get; set; }
+        public string CreatedComputerName { get; set; }
+        public string CreatedIP { get; set; }
+        public string CreatedAdUsername { get; set; }
+        public string CreatedBy { get; set; }
+
+        //Data updated
+        public string UpdatedDate { get; set; }
+        public string UpdatedComputerName { get; set; }
+        public string UpdatedIP { get; set; }
+        public string UpdatedAdUsername { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
--- Investex/Investex.Infrastructure/Context/ApplicationDbContext.cs
+++ Investex/Investex.Infrastructure/Context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Investex.Domain.Entities.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Investex.Infastructure.Context
@@ -9,6 +10,13 @@ namespace Investex.Infastructure.Context
             : base(options)
         {
         }
+
+        public DbSet<NewsArticle> NewsArticles { get; set; }
+        public DbSet<NewsAnalysis> NewsAnalyses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Video> Videos { get; set; }
+
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
